10b887d6d050dea61618821cf23c17356cb21b74
Ayank Kumar <ayank@Ayanks-MacBook-Air.local>
2025-05-04 04:48:59 +0530

    providing environment prod ts file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    // baseUrl: 'https://localhost:8080/api/',
+    // baseUrlMaps: 'https://localhost:8080/maps/',
+
+    baseUrl: '${API_URL}/api/',
+    baseUrlMaps: '${API_URL}/maps/',
+};
