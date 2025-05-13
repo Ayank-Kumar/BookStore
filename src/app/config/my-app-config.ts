d495941bff0fecc6dc15c1e8fbd01ce97fe2702c
Ayank Kumar <ayank@Ayanks-MacBook-Air.local>
2025-05-14 04:56:34 +0530

    changed okta redirection call-back

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oamjrgp4v2lFf6vR5d7',
         issuer: 'https://dev-90538180.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://main.d12q1kcy3nzrqb.amplifyapp.com/login/callback',
         // redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
         prompt: 'login'
